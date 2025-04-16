c902f4f736173b40d727072dc698f9249ba92af1
junsevith <pawelstanik44@gmail.com>
2025-04-16 22:17:54 +0200

    agsdsg

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,52 @@
 #![no_std]
 #![no_main]
 
+use ag_lcd::{Blink, Cursor, LcdDisplay, Lines};
 use panic_halt as _;
+use port_expander::dev::pcf8574::Pcf8574;
+
 
 #[arduino_hal::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
+    let peripherals = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(peripherals);
+    let delay = arduino_hal::Delay::new();
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
+    let sda = pins.a4.into_pull_up_input();
+    let scl = pins.a5.into_pull_up_input();
     let mut led = pins.d13.into_output();
 
-    loop {
+    let i2c_bus = arduino_hal::i2c::I2c::new(peripherals.TWI, sda, scl, 400000);
+    let mut i2c_expander = Pcf8574::new(i2c_bus, true, true, true);
+
+    let mut lcd: LcdDisplay<_, _> = LcdDisplay::new_pcf8574(&mut i2c_expander, delay)
+        .with_cols(16)
+        .with_lines(Lines::TwoLines)
+        // .with_blink(Blink::On)
+        .with_cursor(Cursor::On)
+        .with_reliable_init(10000)
+        .build();
+
+    let slowa = [
+        "baba",
+        "mama",
+        "tata",
+        "kota",
+        "pies",
+        "kot",
+        "piesek",
+        "kotka",
+    ];
+
+    for slowo in slowa {
+        lcd.clear();
+        lcd.print(slowo);
+        arduino_hal::delay_ms(500);
         led.toggle();
-        arduino_hal::delay_ms(1000);
+    }
+
+    loop {
+        arduino_hal::delay_ms(500);
+        led.toggle();
     }
 }
